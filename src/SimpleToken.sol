338ce5e21e0cd093cf9bd8eb6e96db816b731d0c
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-04-01 18:11:19 +0200

    [fix] - Token name

--- src/SimpleToken.sol
+++ src/SimpleToken.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.19;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Simpletoken is ERC20 {
+contract SimpleToken is ERC20 {
     /* State variables */
     string private constant NAME = "SimpleToken";
     string private constant SYMBOL = "ST";
