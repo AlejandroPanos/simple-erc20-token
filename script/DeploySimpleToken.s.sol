43acab54169baf061561d6c085c8517b279d2e58
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-04-01 18:11:39 +0200

    [add] - Deploy script logic

--- script/DeploySimpleToken.s.sol
+++ script/DeploySimpleToken.s.sol
@@ -2,5 +2,17 @@
 pragma solidity ^0.8.19;
 
 import {Script} from "forge-std/Script.sol";
+import {SimpleToken} from "src/SimpleToken.sol";
 
-contract DeploySimpleToken is Script {}
+contract DeploySimpleToken is Script {
+    /* State variables */
+    uint256 private constant INITIAL_SUPPLY = 5000;
+
+    /* Deploy function */
+    function run() public returns (SimpleToken) {
+        vm.startBroadcast();
+        SimpleToken token = new SimpleToken(INITIAL_SUPPLY);
+        vm.stopBroadcast();
+        return token;
+    }
+}
