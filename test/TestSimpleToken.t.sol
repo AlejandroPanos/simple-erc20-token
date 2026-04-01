dd1cbc57787be6b476ba4b291f37d49b2ff627bb
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-04-01 18:15:35 +0200

    [add] - Basic testing logic

--- test/TestSimpleToken.t.sol
+++ test/TestSimpleToken.t.sol
@@ -2,5 +2,22 @@
 pragma solidity ^0.8.19;
 
 import {Test} from "forge-std/Test.sol";
+import {SimpleToken} from "src/SimpleToken.sol";
+import {DeploySimpleToken} from "script/DeploySimpleToken.s.sol";
 
-contract TestSimpleToken is Test {}
+contract TestSimpleToken is Test {
+    /* Instantiate new contracts */
+    SimpleToken private simpleToken;
+    DeploySimpleToken private deployer;
+
+    /* State variables */
+    address john = makeAddr("john");
+    address alice = makeAddr("alice");
+    uint256 private constant VALUE = 5000;
+
+    /* Set up function */
+    function setUp() public {
+        simpleToken = deployer.run();
+        simpleToken.transfer(john, VALUE);
+    }
+}
