825f5de2366fb0f08313bec5f7fe33c091acd9e1
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-04-01 18:07:10 +0200

    [add] - Token logic

--- src/SimpleToken.sol
+++ src/SimpleToken.sol
@@ -1,4 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-contract Simpletoken {}
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Simpletoken is ERC20 {
+    /* State variables */
+    string private constant NAME = "SimpleToken";
+    string private constant SYMBOL = "ST";
+
+    /* Constructor */
+    constructor(uint256 initialSupply) ERC20(NAME, SYMBOL) {
+        _mint(msg.sender, initialSupply);
+    }
+}
